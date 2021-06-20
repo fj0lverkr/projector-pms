c889035016014a12d44632a0680ec3c0b89e58d4
Nils Nahooy <nils.nahooy@live.com>
2021-06-20 15:08:18 +0200

    scaffolded admin route and admin dashboard view

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const userData = require("../util/userData");
+const oktaClient = require("../util/oktaClient");
+
+//Admin root page
+router.get("/", (req, res) => {
+  res.render("admin/dashboard");
+});
+
+module.exports = router;

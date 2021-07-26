c6150a6c7e98c8bc581bc52b1f6e01403b52f7b6
Nils Nahooy <nils.nahooy@live.com>
2021-07-26 14:35:44 +0200

    styling for toasts

--- public/javascripts/toaster.js
+++ public/javascripts/toaster.js
@@ -8,27 +8,35 @@ https://github.com/kamranahmedse/jquery-toast-plugin
 const bgSuccess = "#7DCE94";
 const bgError = "#BD8C7D";
 const bgWarn = "#D1BFA7";
+const bgNotice = "#8E8E90";
 const fgSucces = "#3D3D3F";
 const fgError = "#F6F5F3";
 const fgWarn = "#49494B";
+const fgNotice = "#F9F8FD";
 
-let bg = "";
-let fg = "";
+// Set some defaults
+let bg = bgNotice;
+let fg = fgNotice;
+let bgloader = fgNotice;
 
+// Show toast with default settings for Projector PMS
 function toaster(type, body, header = "") {
   if (header === "") {
     switch (type) {
       case "success":
         header = "Success";
         break;
-      case "warn":
+      case "warning":
         header = "Warning";
         break;
-      case "err":
+      case "error":
         header = "Error";
         break;
+      case "info":
+        header = "Notice";
+        break;
       default:
-        header = "Message";
+        header = "Notice";
         break;
     }
   }
@@ -37,30 +45,42 @@ function toaster(type, body, header = "") {
     case "success":
       bg = bgSuccess;
       fg = fgSucces;
+      bgloader = fgSucces;
       break;
-    case "warn":
+    case "warning":
       bg = bgWarn;
       fg = fgWarn;
+      bgloader = fgWarn;
       break;
-    case "err":
+    case "error":
       bg = bgError;
       fg = fgError;
+      bgloader = fgError;
+      break;
+    case "info":
+      bg = bgNotice;
+      fg = fgNotice;
+      bgloader = fgNotice;
       break;
     default:
-      bg = bgSuccess;
-      fg = fgSucces;
+      bg = bgNotice;
+      fg = fgNotice;
+      bgloader = fgNotice;
       break;
   }
+
   $.toast({
     heading: header,
     text: body,
+    icon: type,
     showHideTransition: "slide",
     bgColor: bg,
     textColor: fg,
     allowToastClose: true,
     hideAfter: 5000,
-    stack: false,
+    stack: true,
     textAlign: "center",
     position: "top-center",
+    loaderBg: bgloader,
   });
 }
